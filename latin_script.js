096245b194ae99da56504b142dbbdcc86cec0133
yamchan <136101905+yamchanman@users.noreply.github.com>
2023-06-12 21:32:45 +0900

    Update latin_script.js
    jQueryを使わないScriptに修正

--- latin_script.js
+++ latin_script.js
@@ -1,7 +1,17 @@
 'use strict';
 
-$(document).ready(function(){
-    $('td').on('click', function(){
-        $(this).find('span').toggleClass('hidden');
-    });
-});
+var cells = document.querySelectorAll('td');
+
+for (var i = 0; i < cells.length; i++) {
+    cells[i].onclick = function() {
+        var span = this.querySelector('span'); // クリックされたセル内の<span>要素を取得
+
+        if (span) {
+          if (span.classList.contains('hidden')) {
+            span.classList.remove('hidden'); // "hidden"クラスがある場合、削除
+          } else {
+            span.classList.add('hidden'); // "hidden"クラスがない場合、追加
+          }
+        }
+      };
+    }
